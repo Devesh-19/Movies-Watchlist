b6b5c82b223af7b4c89eec19694fcbac9f2c3155
Devesh-19 <devdeveshsingh@gmail.com>
2021-07-14 13:15:01 +0530

    Get data from TMDB api

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Add = () => {
-	return <h3>Add Page</h3>;
+	const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+
+	const [query, setQuery] = useState("");
+	const [results, setResults] = useState([]);
+
+	const handleChange = (e) => {
+		const { value } = e.target;
+		setQuery(value);
+	};
+
+	useEffect(() => {
+		if (query) {
+			axios
+				.get(
+					`https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${query}`
+				)
+				.then((res) => {
+					setResults(res.data.results);
+					console.log(results);
+				})
+				.catch((error) => {
+					setResults([]);
+				});
+		}
+	}, [query, API_KEY]);
+
+	return (
+		<div className="add-page">
+			<div className="container">
+				<div className="add-content">
+					<div className="input-wrapper">
+						<input
+							type="text"
+							placeholder="Search for a movie"
+							value={query}
+							onChange={handleChange}
+						/>
+					</div>
+					{results.length > 0 && (
+						<ul>
+							{results.map((result) => (
+								<li>{result.title}</li>
+							))}
+						</ul>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Add;
